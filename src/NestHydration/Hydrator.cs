5047745a73e1cf1bfba535ccf946d60352115075
Rasmus John Pedersen <mail@rjp.dk>
2024-11-29 11:37:20 +0100

    refactor: add entry cache to avoid costly loops

--- src/NestHydration/Hydrator.cs
+++ src/NestHydration/Hydrator.cs
@@ -14,24 +14,28 @@ namespace NestHydration
 
         public IEnumerable<IDictionary<string, object?>> Nest(IEnumerable<IDictionary<string, object?>> dataSet, Definition definition)
         {
-            var result = new List<IDictionary<string, object?>>();
-            var primaryIdColumn = definition.Properties.First(x => x is Property property && property.IsId) as Property;
+            var primaryIdColumn = definition.Properties.FirstOrDefault(x => x is Property property && property.IsId) as Property;
             if (primaryIdColumn == null) throw new InvalidOperationException("Definition did not contain a Property with IsId=true.");
+
+            var result = new List<IDictionary<string, object?>>();
+            var cache = new Dictionary<string, IDictionary<string, object?>>();
 
             foreach (var row in dataSet)
             {
-                var mappedEntry = BuildEntry(primaryIdColumn, row, result);
-                if(mappedEntry == null) continue;
+                var mappedEntry = BuildEntry(primaryIdColumn, row, cache, null, out var isNew);
+                if (mappedEntry is null) continue;
 
+                var parentCacheKey = $"{row[primaryIdColumn.Column]}";
                 foreach (var property in definition.Properties)
                 {
                     if (property is Property pId && pId.IsId) continue;
                     if (property is Property p) Extract(p, row, mappedEntry);
-                    if (property is PropertyObject po) Extract(po, row, mappedEntry);
-                    if (property is PropertyArray pa) Extract(pa, row, mappedEntry);
+                    else if (property is PropertyObject po) Extract(po, row, mappedEntry, cache, parentCacheKey);
+                    else if (property is PropertyArray pa) Extract(pa, row, mappedEntry, cache, parentCacheKey);
                 }
-            }
 
+                if (isNew) result.Add(mappedEntry);
+            }
             return result;
         }
 
@@ -40,7 +44,7 @@ namespace NestHydration
             mappedEntry[property.Name] = row[property.Column];
         }
 
-        private void Extract(PropertyObject propertyObject, IDictionary<string, object?> row, IDictionary<string, object?> mappedEntry)
+        private void Extract(PropertyObject propertyObject, IDictionary<string, object?> row, IDictionary<string, object?> mappedEntry, Dictionary<string, IDictionary<string, object?>> cache, string parentCacheKey)
         {
             var newEntry = new ConcurrentDictionary<string, object?>();
             foreach (var property in propertyObject.Properties)
@@ -52,53 +56,56 @@ namespace NestHydration
                 }
 
                 if (property is Property p) Extract(p, row, newEntry);
-                if (property is PropertyObject po) Extract(po, row, newEntry);
-                if (property is PropertyArray pa) Extract(pa, row, newEntry);
+                else if (property is PropertyObject po) Extract(po, row, newEntry, cache, parentCacheKey);
+                else if (property is PropertyArray pa) Extract(pa, row, newEntry, cache, parentCacheKey);
             }
             mappedEntry[propertyObject.Name] = newEntry;
         }
 
-        private void Extract(PropertyArray propertyArray, IDictionary<string, object?> row, IDictionary<string, object?> mappedEntry)
+        private void Extract(PropertyArray propertyArray, IDictionary<string, object?> row, IDictionary<string, object?> mappedEntry, Dictionary<string, IDictionary<string, object?>> cache, string parentCacheKey)
         {
-            var primaryIdColumn = propertyArray.Properties.First(x => x is Property property && property.IsId) as Property;
+            var primaryIdColumn = propertyArray.Properties.FirstOrDefault(x => x is Property property && property.IsId) as Property;
             if (primaryIdColumn == null) throw new InvalidOperationException("Definition did not contain a Property with IsId=true.");
 
-            if (mappedEntry.TryGetValue(propertyArray.Name, out var exising) is false || exising is not List<IDictionary<string, object?>> list)
-                list = new List<IDictionary<string, object?>>();
+            if (mappedEntry.TryGetValue(propertyArray.Name, out var existing) is false || existing is not List<IDictionary<string, object?>> list)
+                list = new List<IDictionary<string, object>>();
 
-            var mapped = BuildEntry(primaryIdColumn, row, list);
-            if (mapped == null)
+            var mapped = BuildEntry(primaryIdColumn, row, cache, parentCacheKey, out var isNew);
+            if (mapped is null)
             {
                 mappedEntry[propertyArray.Name] = null;
                 return;
             }
 
+            if (isNew) list.Add(mapped);
+
             foreach (var property in propertyArray.Properties)
             {
                 if (property is Property pId && pId.IsId) continue;
                 if (property is Property p) Extract(p, row, mapped);
-                if (property is PropertyObject po) Extract(po, row, mapped);
-                if (property is PropertyArray pa) Extract(pa, row, mapped);
+                else if (property is PropertyObject po) Extract(po, row, mapped, cache, $"{parentCacheKey}__{mapped[primaryIdColumn.Column]}");
+                else if (property is PropertyArray pa) Extract(pa, row, mapped, cache, $"{parentCacheKey}__{mapped[primaryIdColumn.Column]}");
             }
 
-            if(exising is not null) return;
+            if (existing is not null) return;
             mappedEntry[propertyArray.Name] = list;
         }
 
-        private IDictionary<string, object?>? BuildEntry(Property primaryIdColumn, IDictionary<string, object?> row, List<IDictionary<string, object?>> result)
+        private IDictionary<string, object?>? BuildEntry(Property primaryIdColumn, IDictionary<string, object?> row, Dictionary<string, IDictionary<string, object?>> cache, string? parentCacheKey, out bool isNew)
         {
-            var value = row[primaryIdColumn.Column];
-            if (value == null)
+            isNew = false;
+            var id = row[primaryIdColumn.Column];
+            if (id is null)
                 return null;
 
-            var existingEntry = result.FirstOrDefault(x =>
-                x != null && x.ContainsKey(primaryIdColumn.Name) && x.Values.Contains(value));
-            if (existingEntry != null)
-                return existingEntry;
+            var cacheKey = $"{parentCacheKey}__{id}";
+            if (cache.TryGetValue(cacheKey, out var existing))
+                return existing;
 
             var newEntry = new ConcurrentDictionary<string, object?>();
-            newEntry.TryAdd(primaryIdColumn.Name, value);
-            result.Add(newEntry);
+            newEntry[primaryIdColumn.Name] = id;
+            cache.Add(cacheKey, newEntry);
+            isNew = true;
             return newEntry;
         }
     }
